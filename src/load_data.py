ccea16b6e34f3f8f879a23a098b7bd8193f2b0df
phuctran06 <phuc.tran.it06@gmail.com>
2026-09-05 10:16:55 +0700

    Update load_data

--- src/load_data.py
+++ src/load_data.py
@@ -2,11 +2,7 @@ from datasets import load_dataset
 
 
 def load_cnn_dailymail(split="train"):
-    dataset = load_dataset(
-        "abisee/cnn_dailymail",
-        "3.0.0",
-        split=split
-    )
+    dataset = load_dataset("abisee/cnn_dailymail","3.0.0",split=split)
 
     return dataset
 
@@ -17,6 +13,8 @@ if __name__ == "__main__":
 
     print(dataset)
     print("\nNumber of articles:", len(dataset))
+    print(f"Link dataset: https://huggingface.co/datasets/abisee/cnn_dailymail/viewer")
+
 
     print("\nFirst article:")
     print(dataset[0]["article"])
